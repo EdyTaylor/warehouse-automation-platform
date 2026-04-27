1a8c4bc36f4c52f23aee72324dd6b4e1ae13d8cd
EdyTaylor <ed.assassin.ru@gmail.com>
2026-04-27 18:18:29 +0600

    Fix critical 500 error - restore site functionality
    - Restore working products.php from backup
    - Temporarily rename broken products_new.php to products_new.php.broken
    - Add test_syntax.php for debugging
    - Site is now fully operational
    - Will fix products_new.php syntax error separately

--- products.php
+++ products.php
@@ -1,6 +1,11 @@
 <?php
-header('Location: products_new.php');
-exit;
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+header('Content-Type: text/html; charset=utf-8');
+
+require 'db.php';
+$db = getDB();
+require_once __DIR__ . '/api/bitrix/send.php';
 
 function normalizeNumber($value) {
     $value = str_replace(' ', '', $value);
